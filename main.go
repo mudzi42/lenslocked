7b26b09eee5df944aa58447168b4c3376416f7fc
Chip Hudgins <mudzi42@gmail.com>
2024-09-21 20:56:42 -0700

    Home Page via Template

--- main.go
+++ main.go
@@ -2,87 +2,65 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"path/filepath"
+	"text/template"
 
-	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/v5"
 )
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
+	tplPath := filepath.Join("templates", "home.gohtml")
+	tpl, err := template.ParseFiles(tplPath)
+	if err != nil {
+		log.Printf("parsing template: %v", err)
+		http.Error(w, "There was an error parsing the template.", http.StatusInternalServerError)
+		return
+	}
+	err = tpl.Execute(w, nil)
+	if err != nil {
+		log.Printf("executing template: %v", err)
+		http.Error(w, "There was an error executing the template.", http.StatusInternalServerError)
+		return
+	}
 }
 
 func contactHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:mudzi42@gmail.com\">mudzi42@gmail.com</a>.</p>")
+	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:jon@calhoun.io\">jon@calhoun.io</a>.</p>")
 }
 
 func faqHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprint(w, `<h1>FAQ Page</h1>
-	<ul>
-	<li>Q: Is there a free version? A: Yes! We offer a free trial for 30 days on any paid plans.</li>
-	<li>Q: What are your support hours? A: We have support staff answering emails 24/7, though response times may be a bit slower on weekends.</li>
-	<li>Q: How do I contact support? A: Email us - <a href=\"mailto:support@lenslocked.com\">support@lenslocked.com</a>.</li>
-	</ul>
-	`)
-
+<ul>
+  <li>
+    <b>Is there a free version?</b>
+    Yes! We offer a free trial for 30 days on any paid plans.
+  </li>
+  <li>
+    <b>What are your support hours?</b>
+    We have support staff answering emails 24/7, though response
+    times may be a bit slower on weekends.
+  </li>
+  <li>
+    <b>How do I contact support?</b>
+    Email us - <a href="mailto:support@lenslocked.com">support@lenslocked.com</a>
+  </li>
+</ul>
+`)
 }
-func userHandler(w http.ResponseWriter, r *http.Request) {
-	// fetch the url parameter `"userID"` from the request of a matching
-	// routing pattern. An example routing pattern could be: /users/{userID}
-	userID := chi.URLParam(r, "userID")
-
-	// fetch `"key"` from the request context
-	// ctx := r.Context()
-	// key := ctx.Value("key").(string)
-
-	// respond to the client
-	//w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	w.Write([]byte(fmt.Sprintf("hi %v", userID)))
-}
-
-// func pathHandler(w http.ResponseWriter, r *http.Request) {
-// 	switch r.URL.Path {
-// 	case "/":
-// 		homeHandler(w, r)
-// 	case "/contact":
-// 		contactHandler(w, r)
-// 	case "/faq":
-// 		faqHandler(w, r)
-// 	default:
-// 		// TODO: handle the page not found error
-// 		w.WriteHeader(http.StatusNotFound)
-// 		fmt.Fprint(w, "<h1>Page Not Found</h1>")
-// 	}
-// }
-
-// type Router struct{}
-
-// func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-// 	switch r.URL.Path {
-// 	case "/":
-// 		homeHandler(w, r)
-// 	case "/contact":
-// 		contactHandler(w, r)
-// 	case "/faq":
-// 		faqHandler(w, r)
-// 	default:
-// 		// TODO: handle the page not found error
-// 		w.WriteHeader(http.StatusNotFound)
-// 		fmt.Fprint(w, "<h1>Page Not Found</h1>")
-// 	}
-// }
 
 func main() {
 	r := chi.NewRouter()
 	r.Get("/", homeHandler)
 	r.Get("/contact", contactHandler)
 	r.Get("/faq", faqHandler)
-	r.Get("/users", userHandler)
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Page not found", http.StatusNotFound)
 	})
-	fmt.Println("Starting the server on :3000 ...")
+	fmt.Println("Starting the server on :3000...")
 	http.ListenAndServe(":3000", r)
 }
